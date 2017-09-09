d231368c4b89d6c05177aeb16fd6713948c9779e
hadacchi <hadacchi@users.noreply.github.com>
2017-09-10 00:11:11 +0900

    ignore conf.py, add stop script

---
+++ stop.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import os
+import signal
+from conf import PID
+
+if os.path.isfile(PID):
+    with open(PID,'r') as f:
+        pid = int(f.read())
+    os.kill(pid, signal.SIGQUIT)
+    os.remove(PID)
+else:
+    raise Exception('no run.pid file')
